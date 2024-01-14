a0f04d05a28ab95de538ef9175ed44c9557963b6
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-01-14 16:20:11 +0300

    Create BlurView

--- AppleMusic-SwiftUI/View/Modules/TabBarView.swift
+++ AppleMusic-SwiftUI/View/Modules/TabBarView.swift
@@ -2,24 +2,30 @@ import SwiftUI
 
 struct TabBarView: View {
     var body: some View {
-        TabView {
-            LibraryView()
-                .tabItem {
-                    Image(systemName: "play.square.stack")
-                    Text("Library")
-                }
-            Text("Radio")
-                .tabItem {
-                    Image(systemName: "dot.radiowaves.left.and.right")
-                    Text("Radio")
-                }
-            Text("Search")
-                .tabItem {
-                    Image(systemName: "magnifyingglass")
-                    Text("Search")
-                }
+        ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom)) {
+            TabView {
+                LibraryView()
+                    .tabItem {
+                        Image(systemName: "play.square.stack")
+                        Text("Library")
+                    }
+                Text("Radio")
+                    .tabItem {
+                        Image(systemName: "dot.radiowaves.left.and.right")
+                        Text("Radio")
+                    }
+                Text("Search")
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                        Text("Search")
+                    }
+            }
+            .tint(.red)
+            
+            BlurView()
+                .frame(height: 80)
+                .offset(y: -48)
         }
-        .tint(.red)
     }
 }
 
