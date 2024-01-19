1a9385127c563321689aa6feb966fc811066b290
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-01-19 20:26:47 +0300

    Create RadioView

---
+++ AppleMusic-SwiftUI/View/Modules/Rаdio/RadioView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct RadioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RadioView_Previews: PreviewProvider {
+    static var previews: some View {
+        RadioView()
+    }
+}
